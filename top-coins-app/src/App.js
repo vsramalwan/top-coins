0e015608685848d62923ced7ff80c80ceec419a3
Vinod Singh Ramalwan <vinod.ramalwan@smart-data-deutschland.de>
2018-10-08 01:28:41 +0200

    API call for the tickers added

--- top-coins-app/src/App.js
+++ top-coins-app/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { MarketOverviewPage } from "./MarketOverviewPage/MarketOverviewPage";
 import { LiquidityPage } from "./LiquidityPage/LiquidityPage";
-import Header from './Wrapper/Header/Header';
-import Footer from './Wrapper/Footer/Footer';
 
 class App extends Component {
   render() {
--- top-coins-app/src/MarketOverviewPage/MarketOverviewPage.jsx
+++ top-coins-app/src/MarketOverviewPage/MarketOverviewPage.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
+
 import Header from './../Wrapper/Header/Header';
 import Footer from './../Wrapper/Footer/Footer';
 
 export class MarketOverviewPage extends Component {
+  constructor () {
+		super();
+
+		this.state = {
+			tableData: [{
+				name: '',
+				symbol: '',
+				rank: '',
+			}],
+		};
+  }
+  componentDidMount () {
+    axios.get('https://api.coinmarketcap.com/v2/ticker/?sort=rank', {
+      responseType: 'json'
+    }).then(response => {
+        this.setState({ tableData: response.data });
+        console.log(this.state.tableData['data']);
+    });
+  }
   render() {
     return (
       <div>
